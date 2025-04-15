335df0c02dc7e8cf95de9fd236627e2205d35008
evariste060 <nkurunzizaevariste060@gmail.com>
2025-04-15 11:05:08 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-const decrease = document.getElementById("decrease");
-const reset = document.getElementById("reset");
-const increase = document.getElementById("increase");
-const counter = document.getElementById("label");
-let count = 0; 
-increase.onclick = function(){
-    count++;
-    counter.textContent = count;
-}
-decrease.onclick = function(){
-    count--;
-    counter.textContent = count;
-}
-reset.onclick = function(){
-    count = 0;
-    counter.textContent = count;
+
+const button = document.getElementById("button");
+const dice = document.getElementById("label");
+let number;
+let max = 6;
+let min = 1;
+button.onclick = function(){
+    number = Math.floor(Math.random() * max)+ min ;
+    dice.textContent = number;
 }
 
